0d56fd5490a8a10b08c20cd9b6186080ad12025b
Владимир <vkharseev@gmail.com>
2015-07-27 16:20:19 +0300

    API

--- app/modules/application/api.controller.php
+++ app/modules/application/api.controller.php
@@ -12,71 +12,77 @@ class ApiController extends BaseController {
     public static function returnRoutes() {
         $class = __CLASS__;
         Route::group(array('prefix' => 'api'), function () use ($class) {
-            Route::get('help', array('uses' => $class.'@help'));
-            Route::get('help/{method}', array('uses' => $class.'@helpMethod'));
+            Route::get('help', array('uses' => $class . '@help'));
+            Route::get('help/{method}', array('uses' => $class . '@helpMethod'));
 
-            Route::any('register', array('uses' => $class.'@register'));
-            Route::any('questions', array('uses' => $class.'@getQuestions'));
+            Route::any('register', array('uses' => $class . '@register'));
+            Route::any('questions', array('uses' => $class . '@getQuestions'));
         });
     }
 
     /****************************************************************************/
-    public function __construct() {}
+    public function __construct() {
+    }
 
     /****************************************************************************/
-    public static function returnInfo() { }
+    public static function returnInfo() {
+    }
 
-    public static function returnMenu() { }
+    public static function returnMenu() {
+    }
 
-    public static function returnActions() { }
+    public static function returnActions() {
+    }
 
     /****************************************************************************/
-    public function help(){
-        $echo = '<a href="/api/help/register">Метод Register. Добавление пользователя в систему тестирования докторов</a>'."<br>\n";
-        $echo .= '<a href="/api/help/questions">Метод Questions. Получение списка вопросов</a>'."<br>\n";
+    public function help() {
+        $echo = '<a href="/api/help/register">Метод Register. Добавление пользователя в систему тестирования докторов</a>' . "<br>\n";
+        $echo .= '<a href="/api/help/questions">Метод Questions. Получение списка вопросов</a>' . "<br>\n";
         Helper::ta($echo);
     }
-    public function helpMethod($method){
+
+    public function helpMethod($method) {
 
         $echo = '';
         switch ($method):
             case 'register' :
-                $echo = 'Подсказка по методу Register. Добавление пользователя в систему тестирования докторов.'."<br>\n";
-                $echo .= 'Добавление пользователей происходит отправкой данных методами GET или POST.'."<br>\n";
-                $echo .= 'Результат JSON-строка следующего вида:'."<br>\n";
-                $echo .= 'Ошибка - {"error": 1,"data":"_пусто_","message":"_пусто_|E-mail уже зарегистрирован|Неверный токен"}.'."<br>\n";
-                $echo .= 'Успех - {"error": 0,"data":"_пусто_","message":"Аккаунт зарегистрирован"}.'."<br><br>\n\n";
-                $echo .= 'Пример вызова API с GET параметрами:'."<br>\n";
-                $echo .= '/api/register?token='.md5('user@doctornarabote.ru'.'doctor_on_work_testing').'&remote_id=1&email=user@doctornarabote.ru&name=Доктор'."<br>\n";
-                $echo .= 'Где:'."<br>\n";
-                $echo .= '1) token = md5(email.secret_string). secret_string = секретная строка. email = E-mail пользователя ' ."<br>\n";
-                $echo .= '2) remote_id = ID пользователя сайта'."<br>\n";
-                $echo .= '3) email = E-mail пользователя'."<br>\n";
+                $echo = 'Подсказка по методу Register. Добавление пользователя в систему тестирования докторов.' . "<br>\n";
+                $echo .= 'Добавление пользователей происходит отправкой данных методами GET или POST.' . "<br>\n";
+                $echo .= 'Результат JSON-строка следующего вида:' . "<br>\n";
+                $echo .= 'Ошибка - {"error": 1,"data":"_пусто_","message":"_пусто_|E-mail уже зарегистрирован|Неверный токен"}.' . "<br>\n";
+                $echo .= 'Успех - {"error": 0,"data":"_пусто_","message":"Аккаунт зарегистрирован"}.' . "<br><br>\n\n";
+                $echo .= 'Пример вызова API с GET параметрами:' . "<br>\n";
+                $echo .= '/api/register?token=' . md5('user@doctornarabote.ru' . 'doctor_on_work_testing') . '&remote_id=1&email=user@doctornarabote.ru&name=Доктор' . "<br>\n";
+                $echo .= 'Где:' . "<br>\n";
+                $echo .= '1) token = md5(email.secret_string). secret_string = секретная строка. email = E-mail пользователя ' . "<br>\n";
+                $echo .= '2) remote_id = ID пользователя сайта' . "<br>\n";
+                $echo .= '3) email = E-mail пользователя' . "<br>\n";
                 $echo .= '4) name - Имя пользователя';
                 break;
             case 'questions':
-                $echo = 'Подсказка по методу Questions. Получение списка вопросов.'."<br>\n";
-                $echo .= 'Получение списка вопросов происходит отправкой данных методами GET или POST.'."<br>\n";
-                $echo .= 'Результат JSON-строка следующего вида:'."<br>\n";
-                $echo .= 'Ошибка - {"error": 1,"data":"_пусто_","message":"_пусто_|Неверный токен"}.'."<br>\n";
-                $echo .= 'Успех - {"error": 0,"data":"Массив данных","message":"_пусто_"}.'."<br>\n";
-                $echo .= 'Где data = [question: string (Текст вопроса), is_true: integer 0|1 (Это правда), answer: string (Текст правильного ответа), is_branding : integer 0|1 (Брендированный)]'."<br><br>\n\n";
-                $echo .= 'Пример вызова API с GET параметрами:'."<br>\n";
-                $echo .= '/api/questions?token='.md5('questions'.'doctor_on_work_testing')."<br>\n";
-                $echo .= 'Где:'."<br>\n";
-                $echo .= 'token = md5("questions".secret_string). secret_string = секретная строка. "questions" = строка questions ' ."<br>\n";
+                $echo = 'Подсказка по методу Questions. Получение списка вопросов.' . "<br>\n";
+                $echo .= 'Получение списка вопросов происходит отправкой данных методами GET или POST.' . "<br>\n";
+                $echo .= 'Результат JSON-строка следующего вида:' . "<br>\n";
+                $echo .= 'Ошибка - {"error": 1,"data":"_пусто_","message":"_пусто_|Неверный токен"}.' . "<br>\n";
+                $echo .= 'Успех - {"error": 0,"data":"Массив данных","message":"_пусто_"}.' . "<br>\n";
+                $echo .= 'Где data = [question: string (Текст вопроса), is_true: integer 0|1 (Это правда), answer: string (Текст правильного ответа), is_branding : integer 0|1 (Брендированный)]' . "<br><br>\n\n";
+                $echo .= 'Пример вызова API с GET параметрами:' . "<br>\n";
+                $echo .= '/api/questions?token=' . md5('questions' . 'doctor_on_work_testing') . "<br>\n";
+                $echo .= 'Где:' . "<br>\n";
+                $echo .= 'token = md5("questions".secret_string). secret_string = секретная строка. "questions" = строка questions ' . "<br>\n";
                 break;
         endswitch;
         Helper::ta($echo);
     }
+
     /****************************************************************************/
-    public function register(){
+    public function register() {
 
         $validator = Validator::make(Input::all(), array('token' => 'required', 'remote_id' => 'required|numeric',
             'email' => 'required|email', 'name' => 'required', 'password' => ''));
         if ($validator->passes()):
             $post = Input::all();
-            if($post['token'] == md5($post['email'].Config::get('doktornarabote.secret_string'))):
+            if ($post['token'] == md5($post['email'] . Config::get('doktornarabote.secret_string'))):
                 if (User::where('email', $post['email'])->exists() === FALSE):
                     $user = new User;
                     $user->remote_id = $post['remote_id'];
@@ -98,12 +104,12 @@ class ApiController extends BaseController {
         return Response::json($this->json_request, 200);
     }
 
-    public function getQuestions(){
+    public function getQuestions() {
 
         $validator = Validator::make(Input::all(), array('token' => 'required'));
         if ($validator->passes()):
             $post = Input::all();
-            if($post['token'] == md5('questions'.Config::get('doktornarabote.secret_string'))):
+            if ($post['token'] == md5('questions' . Config::get('doktornarabote.secret_string'))):
                 $questions = array();
                 foreach (Questions::orderBy('order')->get() as $question):
                     $questions[] = array(
