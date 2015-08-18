e580378c21fd1d54b0c22c5ce44a94aef059a574
Владимир <vkharseev@gmail.com>
2015-08-18 11:41:03 +0300

    API без md5

--- app/modules/application/api.controller.php
+++ app/modules/application/api.controller.php
@@ -52,9 +52,9 @@ class ApiController extends BaseController {
                 $echo .= 'Ошибка - {"error": 1,"data":"_пусто_","message":"_пусто_|E-mail уже зарегистрирован|Неверный токен"}.' . "<br>\n";
                 $echo .= 'Успех - {"error": 0,"data":"_пусто_","message":"Аккаунт зарегистрирован"}.' . "<br><br>\n\n";
                 $echo .= 'Пример вызова API с GET параметрами:' . "<br>\n";
-                $echo .= '/api/register?token=' . md5('_user@doctornarabote.ru_' . 'doctor_on_work_testing') . '&remote_id=1&email=user@doctornarabote.ru&name=Доктор' . "<br>\n";
+                $echo .= '/api/register?token=doctor_on_work_testing&remote_id=1&email=user@doctornarabote.ru&name=Доктор' . "<br>\n";
                 $echo .= 'Где:' . "<br>\n";
-                $echo .= '1) token = md5(email.secret_string). secret_string = секретная строка. email = E-mail пользователя ' . "<br>\n";
+                $echo .= '1) token = secret_string = секретная строка. email = E-mail пользователя ' . "<br>\n";
                 $echo .= '2) remote_id = ID пользователя сайта' . "<br>\n";
                 $echo .= '3) email = E-mail пользователя' . "<br>\n";
                 $echo .= '4) name - Имя пользователя';
@@ -67,13 +67,13 @@ class ApiController extends BaseController {
                 $echo .= 'Успех - {"error": 0,"data":"Массив данных","message":"_пусто_"}.' . "<br>\n";
                 $echo .= 'Где data = [question: string (Текст вопроса), doctor_type : integer (Номер доктора), is_true: integer 0|1 (Это правда), answer: string (Текст правильного ответа), is_branding : integer 0|1 (Брендированный)]' . "<br><br>\n\n";
                 $echo .= 'Пример вызова API с GET параметрами:' . "<br>\n";
-                $echo .= '/api/questions?token=' . md5('_questions_' . 'doctor_on_work_testing') . "&doctor=1<br>\n";
+                $echo .= "/api/questions?token=doctor_on_work_testing&doctor=1<br>\n";
                 $echo .= 'Где:' . "<br>\n";
                 $echo .= 'token = md5("questions".secret_string). secret_string = секретная строка. "questions" = строка questions ' . "<br>\n";
                 $echo .= 'doctor - номер доктора или его имя. Возможные значения: 1 или уролог, 2 или гинеколог.'."<br>\n";
                 $echo .= 'Например:'."<br>\n";
-                $echo .= '/api/questions?token=' . md5('_questions_' . 'doctor_on_work_testing') . "&doctor=2<br>\n";
-                $echo .= '/api/questions?token=' . md5('_questions_' . 'doctor_on_work_testing') . "&doctor=уролог<br>\n";
+                $echo .= "/api/questions?token=doctor_on_work_testing&doctor=2<br>\n";
+                $echo .= "/api/questions?token=doctor_on_work_testing&doctor=уролог<br>\n";
                 $echo .= 'Внимание. Если не передавать параметр doctor в ответе будет полный список вопросов'."\n";
                 break;
         endswitch;
@@ -87,7 +87,7 @@ class ApiController extends BaseController {
             'email' => 'required|email', 'name' => 'required', 'password' => ''));
         if ($validator->passes()):
             $post = Input::all();
-            if ($post['token'] == md5($post['email'] . Config::get('doktornarabote.secret_string'))):
+            if ($post['token'] == Config::get('doktornarabote.secret_string')):
                 if (User::where('email', $post['email'])->exists() === FALSE):
                     $user = new User;
                     $user->remote_id = $post['remote_id'];
@@ -114,7 +114,7 @@ class ApiController extends BaseController {
         $validator = Validator::make(Input::all(), array('token' => 'required', 'doctor'=>''));
         if ($validator->passes()):
             $post = Input::all();
-            if ($post['token'] == md5('questions' . Config::get('doktornarabote.secret_string'))):
+            if ($post['token'] == Config::get('doktornarabote.secret_string')):
                 $questions = array();
                 if(Input::has('doctor')):
                     $doctor_type = Input::get('doctor');
